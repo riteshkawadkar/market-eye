eb1e95760ce468e9701296b6a55c62732f53c37f
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2021-06-30 16:14:02 +0530

    Added Media in Settings

--- project/settings.py
+++ project/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/img/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
