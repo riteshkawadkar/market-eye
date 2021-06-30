12f75470aa179b09540a5bc6f588e272a8479c4c
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2021-06-30 15:45:27 +0530

    Removed Additnal html

--- project/settings.py
+++ project/settings.py
@@ -137,9 +137,6 @@ STATICFILES_DIRS = (
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
